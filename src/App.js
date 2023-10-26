6f944de47af8fc55371c8bcd9f4f66b98b795980
fkdkskdfjfk <yljw225@naver.com>
2023-10-26 21:39:01 +0900

    togo challenge start

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+import reset from "styled-reset";
+
+
+const GlobalStyle = createGlobalStyle`
+  ${reset}
+
+  /* 글로벌 스타일 */
+
+`;
+
+function App() {
+
+  
+  return (
+    <>
+    
+    </>
+  );
+}
+
+export default App;
